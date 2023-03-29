6ed27726e0e1ada33301c2d7155bd6ca3f9bd568
chsir <chsir@163.com>
2023-03-29 13:38:02 +0800

    feature:使用静态路由的中间件

--- app/http/route.go
+++ app/http/route.go
@@ -3,9 +3,10 @@ package http
 import (
 	"github.com/chsir-zy/anan/app/http/module/demo"
 	"github.com/chsir-zy/anan/framework/gin"
+	"github.com/chsir-zy/anan/framework/middleware/static"
 )
 
 func Routes(r *gin.Engine) {
-	r.Static("/dist", "./dist/")
+	r.Use(static.Serve("/", static.LocalFile("./dist", false)))
 	demo.Register(r)
 }
